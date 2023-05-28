26928b39153525257d41322e7cec8129da119b03
Jasen <60624556+jasenscode@users.noreply.github.com>
2023-05-28 20:58:11 +0100

    style: :art: prettier formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import "./globals.css";
+import './globals.css';
 
-import { Container } from "@components/Container";
-import { Navigation } from "@components/Nav";
+import { Container } from '@components/Container';
+import { Navigation } from '@components/Nav';
 
 export const metadata = {
-  title: "Share Seek",
-  description: "Market data at your fingertips",
+  title: 'Share Seek',
+  description: 'Market data at your fingertips',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="bg-violet">
+    <html lang='en'>
+      <body className='bg-violet'>
         <Navigation />
         {children}
       </body>
